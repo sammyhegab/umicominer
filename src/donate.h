bfe8dbbd07ccb333f68ef7c974336886c5a3de6d
Sammy <sammy.hegab@outlook.com>
2022-01-02 13:56:30 +0000

    set donation to 0

--- src/donate.h
+++ src/donate.h
@@ -22,23 +22,7 @@
 
 /*
  * Dev donation.
- *
- * Percentage of your hashing power that you want to donate to the developer, can be 0 if you don't want to do that.
- *
- * Example of how it works for the setting of 1%:
- * You miner will mine into your usual pool for random time (in range from 49.5 to 148.5 minutes),
- * then switch to the developer's pool for 1 minute, then switch again to your pool for 99 minutes
- * and then switch again to developer's pool for 1 minute, these rounds will continue until miner working.
- *
- * Randomised only first round, to prevent waves on the donation pool.
- *
- * Switching is instant, and only happens after a successful connection, so you never loose any hashes.
- *
- * If you plan on changing this setting to 0 please consider making a one off donation to my wallet:
- * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
  */
-constexpr const int kDefaultDonateLevel = 1;
-constexpr const int kMinimumDonateLevel = 1;
-
-
-#endif /* XMRIG_DONATE_H */
+constexpr const int kDefaultDonateLevel = 0;
+constexpr const int kMinimumDonateLevel = 0;
+#endif 
